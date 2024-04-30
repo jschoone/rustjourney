56098e38e7778b8e74702117001cca604d1faf01
Jan Schoone <6106846+jschoone@users.noreply.github.com>
2024-04-30 15:51:05 +0200

    feat(playground): change to structs
    Signed-off-by: Jan Schoone <6106846+jschoone@users.noreply.github.com>

--- book/playground/src/main.rs
+++ book/playground/src/main.rs
@@ -1,22 +1,54 @@
+use std::u8;
+
+struct Pokemon {
+    name: String,
+    level: u8,
+    pokemon_type: String,
+    hp: u8,
+}
+
+impl Pokemon {
+    fn print_pokemon(&self) {
+        println!("Name: {}", self.name);
+        println!("Level: {}", self.level);
+        println!("Type: {}", self.pokemon_type);
+        println!("HP: {}", self.hp);
+    }
+
+    // Changing name makes no sense for Pokemon but it's for learning. (:
+    fn change_name(&mut self, new_name: String) {
+        self.name = new_name;
+    }
+
+    fn increase_level(&mut self) {
+        self.level += 1;
+    }
+
+    fn decrease_hp(&mut self, drain: u8) {
+        self.hp -= drain;
+    }
+}
+
 fn main() {
-    let pikachu_name = String::from("Pikachu");
-    print_pokemon(pikachu_name);
-    // print_pokemon(pikachu_name); // Since pikachu_name has been moved to print_pokemon this is
-    // no longer available
-    let pikachu_name = String::from("Pikachu");
-    print_pokemon_reference(&pikachu_name);
-    print_pokemon_reference(&pikachu_name); // this is possible because only the reference is
-                                            // passed to the function
-
-    print_pokemon(pikachu_name.clone());
-    print_pokemon(pikachu_name.clone()); // this works also because the value will be cloned before
-                                         // passing to the function
+    let mut pikachu = Pokemon {
+        name: String::from("Pikachu"),
+        level: 25,
+        pokemon_type: String::from("Electric"),
+        hp: 100,
+    };
+    print_pokemon(&pikachu);
+    pikachu.print_pokemon();
+    pikachu.change_name(String::from("Pika"));
+    pikachu.print_pokemon();
+    pikachu.increase_level();
+    pikachu.print_pokemon();
+    pikachu.decrease_hp(20);
+    pikachu.print_pokemon();
 }
 
-fn print_pokemon(s: String) {
-    println!("Name: {}", s);
+fn print_pokemon(p: &Pokemon) {
+    println!("Name: {}", p.name);
+    println!("Level: {}", p.level);
+    println!("Type: {}", p.pokemon_type);
 }
 
-fn print_pokemon_reference(s: &String) {
-    println!("Name: {}", s);
-}
