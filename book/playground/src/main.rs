d9bd5b4e350a609ca95b5fec6a97e9d339f0b4ef
Jan Schoone <6106846+jschoone@users.noreply.github.com>
2024-05-02 21:48:10 +0200

    feat(playground): add enums/chapter06
    Signed-off-by: Jan Schoone <6106846+jschoone@users.noreply.github.com>

--- book/playground/src/main.rs
+++ book/playground/src/main.rs
@@ -1,9 +1,14 @@
 use std::u8;
 
+enum PokemonType {
+    Electric,
+    Water,
+}
+
 struct Pokemon {
     name: String,
     level: u8,
-    pokemon_type: String,
+    pokemon_type: PokemonType,
     hp: u8,
 }
 
@@ -11,8 +16,15 @@ impl Pokemon {
     fn print_pokemon(&self) {
         println!("Name: {}", self.name);
         println!("Level: {}", self.level);
-        println!("Type: {}", self.pokemon_type);
+        println!("Type: {}", self.print_pokemon_type());
         println!("HP: {}", self.hp);
+    }
+
+    fn print_pokemon_type(&self) -> String {
+        match &self.pokemon_type {
+            PokemonType::Electric => String::from("Electric"),
+            PokemonType::Water => String::from("Water"),
+        }
     }
 
     // Changing name makes no sense for Pokemon but it's for learning. (:
@@ -27,15 +39,25 @@ impl Pokemon {
     fn decrease_hp(&mut self, drain: u8) {
         self.hp -= drain;
     }
+
+    fn new(name: String, level: u8, pokemon_type: PokemonType, hp: u8) -> Self {
+        Pokemon {
+            name,
+            level,
+            pokemon_type,
+            hp,
+        }
+    }
 }
 
 fn main() {
     let mut pikachu = Pokemon {
         name: String::from("Pikachu"),
         level: 25,
-        pokemon_type: String::from("Electric"),
+        pokemon_type: PokemonType::Electric,
         hp: 100,
     };
+    let shiggy = Pokemon::new(String::from("Shiggy"), 10, PokemonType::Water, 30);
     print_pokemon(&pikachu);
     pikachu.print_pokemon();
     pikachu.change_name(String::from("Pika"));
@@ -44,11 +66,11 @@ fn main() {
     pikachu.print_pokemon();
     pikachu.decrease_hp(20);
     pikachu.print_pokemon();
+    shiggy.print_pokemon();
 }
 
 fn print_pokemon(p: &Pokemon) {
     println!("Name: {}", p.name);
     println!("Level: {}", p.level);
-    println!("Type: {}", p.pokemon_type);
+    println!("HP: {}", p.hp)
 }
-
