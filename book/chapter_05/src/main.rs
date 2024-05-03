7f1223c03b4e89ee55d2a4202a817cb850d6229d
Jan Schoone <6106846+jschoone@users.noreply.github.com>
2024-05-03 08:49:55 +0200

    feat(chapter05): add example from book
    Signed-off-by: Jan Schoone <6106846+jschoone@users.noreply.github.com>

---
+++ book/chapter_05/src/main.rs
@@ -0,0 +1,44 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+
+    fn new(width: u32, height: u32) -> Self {
+        Self { width, height }
+    }
+
+    fn can_hold(&self, other: &Self) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    let rect2 = Rectangle::new(10, 40);
+    let rect3 = Rectangle::new(60, 45);
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+
+    if rect1.width() {
+        println!("The rectangle has a nonzero width; it is {}", rect1.width);
+    }
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+}
